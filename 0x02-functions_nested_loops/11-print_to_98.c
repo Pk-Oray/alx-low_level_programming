6f41891bc3ec38bd4d9278553af75d18a431f737
Pk-Oray <pkowiredu@outlook.com>
2023-02-22 15:31:25 -0800

    checked error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,12 @@
 #include "main.h"
+
 /**
- * print_to_98 - Prints all natural numbers
- * from n to 98.
+ * main - check the code for Holberton School students.
  *
- * @n: input number.
- *
- * Return: no return.
+ * Return: Always 0.
  */
-void print_to_98(int n)
+int main(void)
 {
-	if (n > 98)
-	{
-		for (; n > 98; n--)
-		{
-			printf("%d, ", n);
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
-	}
-	printf("%d\n", n);
+	print_to_98(98);
+	return (0);
 }
