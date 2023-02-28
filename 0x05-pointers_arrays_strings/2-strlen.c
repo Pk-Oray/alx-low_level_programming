416620a47d14d4e4b1473d3325b21c632ff08fa9
Pk-Oray <pkowiredu@outlook.com>
2023-02-27 17:37:31 -0800

    final push

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,20 @@
 #include "main.h"
+
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * _strlen - Returns the length of a string.
+ * @s: String.
+ *
+ * Return: Length of string.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t lenght = 0;
-	while (*str++)
-		lenght++;
-	return (lenght);
+        int longi = 0;
+
+        while (*s != '\0')
+        {
+                longi++;
+                s++;
+        }
+
+        return (longi);
 }
