6672e4bffb753feb988c427a723009a3f6ca5963
Pk-Oray <pkowiredu@outlook.com>
2023-03-01 18:04:55 -0800

    correct push

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,10 @@ char *_strncat(char *dest, const char *src, int n)
 	int dest_len = 0, i;
 
 	while (dest[dest_len] != '\0')
-		dest_len++;
 
+	dest_len++;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-
+	dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
-
 	return (dest);
 }
